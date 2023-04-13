e98d5ac6d95bdda5c52b01621f8649f24f932659
SkylarShi <yus029@ucsd.edu>
2023-04-12 17:25:17 -0700

    fix wording and delete unused lines

--- app.js
+++ app.js
@@ -183,7 +183,6 @@ var question1=function(filePath){
         myCircle
             .transition()
             .duration(duration_1)
-            //.attr("x", function(d) { return xScale(d[1]); })
             .attr("cx", function(d) { return xScale(d[1]); })
 
         myLine
@@ -709,28 +708,6 @@ var question4=function(filePath){
         .style("opacity", 0)
         .attr("class", "tooltip")
         .style("font-size", "16px")
-        // Three function that change the tooltip when user hover / move / leave a cell
-        // var mouseover = function(d) {
-        // tooltip
-        // .transition()
-        // .duration(200)
-        // .style("opacity", 1)
-        // tooltip
-        //     .html("<span style='color:grey'>Sepal length: </span>" + d.Sepal_Length) // + d.Prior_disorder + "<br>" + "HR: " +  d.HR)
-        //     .style("left", (d3.mouse(this)[0]+30) + "px")
-        //     .style("top", (d3.mouse(this)[1]+30) + "px")
-        // }
-        // var mousemove = function(d) {
-        // tooltip
-        // .style("left", (d3.mouse(this)[0]+30) + "px")
-        // .style("top", (d3.mouse(this)[1]+30) + "px")
-        // }
-        // var mouseleave = function(d) {
-        // tooltip
-        // .transition()
-        // .duration(200)
-        // .style("opacity", 0)
-        // }
 
         // Add individual points with jitter
         var jitterWidth = 50
